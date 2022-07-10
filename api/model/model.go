ca18cf06965fa187cd601e0f0b9e00d2e96ddfbb
kaji <kajiharalivi@icloud.com>
2022-07-10 15:39:31 +0900

    model.go

--- api/model/model.go
+++ api/model/model.go
@@ -1,2 +1,21 @@
 package model
 
+type ResponseJson struct {
+	Responses Location `json:"response"`
+}
+
+type Location struct {
+	Location []LocationDetail `json:"location"`
+}
+
+type LocationDetail struct {
+	Postal     string `json:"postal"`
+}
+
+type ResponseErrorJson struct {
+	Response ErrorDetail `json:"response"`
+}
+
+type ErrorDetail struct {
+	Error string `json:"error"`
+}
